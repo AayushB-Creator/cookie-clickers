9d5ac0c7d7b6b49dc9d5db2447311eceb5a35eaa
Aayush Bharti <aayushb22.cse@gmail.com>
2025-12-19 19:26:03 +0530

    project complete

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ let cookieBtn = document.getElementById("cookie")
 let counterDisplay = document.getElementById('counter')
 let grandmotherBtn = document.getElementById('item-1')
 let cpsDisplay= document.getElementById('cps')
-let cookieCount =0
-let cookiesPerSecond=0
+let bakerBtn= document.getElementById('item-2')
+let factoryBtn= document.getElementById('item-3')
+let cookieCount =Number(localStorage.getItem('cookiecount'))
+let cookiesPerSecond=Number(localStorage.getItem('cookiespersecond'))
+cpsDisplay.innerText=`${cookiesPerSecond}/s`
 counterDisplay.innerText= cookieCount
 cookieBtn.addEventListener('click',function(){
     cookieCount++
@@ -11,11 +14,46 @@ cookieBtn.addEventListener('click',function(){
 })
 
 grandmotherBtn.addEventListener('click',function(){
-    cookiesPerSecond = cookiesPerSecond+1
-    cpsDisplay.innerText=`${cookiesPerSecond}/s`
+    if(cookieCount>=100){
+        cookieCount-=100
+        counterDisplay.innerText=cookieCount
+        cookiesPerSecond = cookiesPerSecond+1
+        cpsDisplay.innerText=`${cookiesPerSecond}/s`
+    }else{
+        alert('can not afford right now!')
+    }
+})
+
+bakerBtn.addEventListener('click',function(){
+    if(cookieCount>=500){
+        cookieCount-=500
+        counterDisplay.innerText=cookieCount
+        cookiesPerSecond=cookiesPerSecond+10
+        cpsDisplay.innerText=`${cookiesPerSecond}/s`
+    }else{
+        alert('can not afford right now!')
+    }
+})
+
+factoryBtn.addEventListener('click',function(){
+    if(cookieCount>=5000){
+        cookieCount-=5000
+        counterDisplay.innerText=cookieCount
+        cookiesPerSecond+=100
+        cpsDisplay.innerText=`${cookiesPerSecond}/s`
+    }else{
+        alert('can not afford right now!')
+    }
 })
 
 setInterval(function(){
     cookieCount=cookieCount+cookiesPerSecond
     counterDisplay.innerText=cookieCount
+    saveGame()
 }, 1000);
+
+function saveGame(){
+    localStorage.setItem('cookiecount',cookieCount)
+
+    localStorage.setItem('cookiespersecond',cookiesPerSecond)
+}
