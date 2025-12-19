5ae8b3388f123ac9675b566d0724bd2970416c83
Aayush Bharti <aayushb22.cse@gmail.com>
2025-12-19 18:32:39 +0530

    update 2

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 let cookieBtn = document.getElementById("cookie")
 let counterDisplay = document.getElementById('counter')
+let grandmotherBtn = document.getElementById('item-1')
 
+let cookieCount =0
+counterDisplay.innerText= cookieCount
 cookieBtn.addEventListener('click',function(){
-    
+    cookieCount++
+    counterDisplay.innerText=cookieCount
 })
+setInterval(function(){
+    cookieCount=cookieCount+0
+    counterDisplay.innerText=cookieCount
+}, 1000);
